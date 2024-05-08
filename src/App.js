71b765325239a41f4caf07be2ec6c0edb29e50e4
Layomi1 <107419093+Layomi1@users.noreply.github.com>
2024-05-08 13:38:09 +0100

    implenting Qr code with React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import QRCode from "react-qr-code";
+import "./App.css";
 
 function App() {
+  const [qrCode, setQrCode] = useState("");
+  const [input, setInput] = useState("");
+
+  function handleGenerateQRCode() {
+    setQrCode(input);
+    setInput("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="top">
+        <h1>QR code Generator</h1>
+        <div>
+          <label htmlFor="text"></label>
+          <input
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            name="qr-code"
+            value={input}
+            placeholder="Enter something"
+          />
+          <button
+            disabled={input && input.trim() !== "" ? false : true}
+            onClick={handleGenerateQRCode}
+          >
+            Generate Code
+          </button>
+        </div>
+      </div>
+
+      <div>
+        <QRCode id="qr-code-value" value={qrCode} bgColor="#fff" size={400} />
+      </div>
     </div>
   );
 }
